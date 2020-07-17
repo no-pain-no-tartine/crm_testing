ad547a41a7c2a983f88f5db1fca05220cbe9bb18
Jules Pontois <julespontois@outlook.com>
2020-07-17 18:48:52 +0200

    Install Mongoose/MongoDb/Nodemon/Jest dependencies + Edit Docker for MongoDB

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const http = require('http');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/crm_db', { useNewUrlParser: true })
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('conected to mongoDB');
+});
 
 const server = http.createServer(function(req, res) {
     res.writeHead(200);
-    res.end('Ca marche');
+    res.end('Ca marche chef !');
 });
 server.listen(3000);
