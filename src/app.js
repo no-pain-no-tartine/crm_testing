bc7ba0db773811e3e3c260f935f0ad5d18094814
Jules Pontois <julespontois@outlook.com>
2020-07-19 09:54:39 +0200

    Link app to mongo DB

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/crm_db', { useNewUrlParser: true })
+//mongoose.connect('mongodb://' + process.env['DB_CONTAINER'] + '/' + process.env.DB_NAME, { useNewUrlParser: true })
+mongoose.connect('mongodb://mongo/crm_db', { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
