7eba1c7be0eaa52366f785c6f9a3a52dd242b7ac
Jules Pontois <julespontois@outlook.com>
2020-07-19 23:36:39 +0200

    Integrating handlebar views 1.2

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -62,8 +62,7 @@ exports.createContactValid = async (req, res) => {
 };
 
 exports.updateContactForm = (req, res) => {
-    const contactID = {_id: req.params.id}
-    contactModel.findOneAndUpdate(contactID, newContact, {new: true}, (error, newContact) => {
+    contactModel.findById({_id: req.params.id}, (error, newContact) => {
         if (error) {
             res.status(500);
             console.log(error);
@@ -71,7 +70,9 @@ exports.updateContactForm = (req, res) => {
             res.status(200);
             res.render('form', {
                 title: 'Modifier un contact',
-                action: 'update'
+                action: 'update',
+                id: req.params.id,
+                contact: newContact
             });
         }
     });
@@ -79,30 +80,37 @@ exports.updateContactForm = (req, res) => {
 
 exports.updateContactValid = (req, res) => {
     const validation = validation(req.body);
-    if(validation.isError) return res.status(400).json({error: validation.message});
+    if(validation.isError) {
+        res.render('form', {
+            title: 'Modifier un contact',
+            action: 'update',
+            id: req.params.id,
+            errors: validation.errors
+        });
+    } else {
+        const newContact = new contact ({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email:req.body.email,
+            noPhone:req.body.noPhone,
+            tag:req.body.tag
+        });
 
-    const newContact = new contact ({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email:req.body.email,
-        noPhone:req.body.noPhone,
-        tag:req.body.tag
-    });
-
-    const contactID = {
-        _id: req.params.id
-    }
-
-    contactModel.findOneAndUpdate(contactID, newContact, {new: true}, (error, newContact) => {
-        if (error) {
-            res.status(500);
-            console.log(error);
-            res.json({message: "Server error"});
-        } else {
-            res.status(200);
-            res.json(newContact);
+        const contactID = {
+            _id: req.params.id
         }
-    });
+
+        contactModel.findOneAndUpdate(contactID, newContact, {new: true}, (error, newContact) => {
+            if (error) {
+                res.status(500);
+                console.log(error);
+                res.json({message: "Server error"});
+            } else {
+                res.status(200);
+                res.redirect('/');
+            }
+        });
+    }
 }
 
 exports.deleteContact = (req, res) => {
